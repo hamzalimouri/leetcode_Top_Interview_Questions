44c6f5218599d490133e7b5b23edfa85035ab37f
MiraBou <m.boussarendal@gmail.com>
2021-09-18 18:15:47 +0100

    New branch

--- Array and Strings/Single Number/singleNumber.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        d = {}
-        for num in nums:
-            d[num] = d.get(num, 0) + 1
-        # return list(d.keys())[list(d.values()).index(1)]
-        for key, value in d.items():
-            if value == 1:
-                return key
